964933b2df7c0f4a10cbf28d5b4c173de3a413a5
jaeyoung3806 <jaeyoung3806@naver.com>
2024-02-07 17:50:10 +0900

    feat:javascript,css

---
+++ array_study/js/test.js
@@ -0,0 +1,11 @@
+
+let arr = new Array();
+
+
+function inputValue(text) {
+    const text = document.querySelector("#input-value").value;
+    
+    console.log(text);
+    arr.push(text.Value);
+    console.log(arr);
+}
